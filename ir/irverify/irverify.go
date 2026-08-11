cbc65c8d2e7706fcfaa44cf9ead4d8592f53b7df
Omar Aljarrah <50204418+OmarAlJarrah@users.noreply.github.com>
2026-08-11 20:46:00 +0300

    feat(irverify): reject a union that declares no variants (#360)
    PR: https://github.com/dexpace/morphic/pull/360

--- ir/irverify/irverify.go
+++ ir/irverify/irverify.go
@@ -33,6 +33,7 @@ func Verify(doc *ir.Document) []Violation {
 	vs = append(vs, checkPrimIDs(doc)...)
 	vs = append(vs, checkPrimKinds(doc)...)
 	vs = append(vs, checkAuthKinds(doc)...)
+	vs = append(vs, checkUnions(doc)...)
 	vs = append(vs, checkDiagnostics(doc)...)
 	vs = append(vs, checkVersion(doc)...)
 	vs = append(vs, runWalkChecks(doc)...)
